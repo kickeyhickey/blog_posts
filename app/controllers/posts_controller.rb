86a5ed1cb9da39146dcbe66a09a0a6f6ec13929a
kickeyhickey <hickeydeveloper@gmail.com>
2022-05-25 21:53:30 -0700

    added index view. Started reformat of controller from json to html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
 
     def index
-        posts = Post.all
-        render json: posts
+        @posts = Post.all
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
-  get '/posts' => 'posts#index'
+  resources :posts
   get 'posts/:id' => 'posts#show'
   post 'posts' => 'posts#create'
   patch 'posts/:id' => 'posts#update'
   delete 'posts/:id' => 'posts#destroy'
+  root 'posts#index'
 end
