f05dd0abd16f10e528c47f0d66cc5879db1600bb
kickeyhickey <hickeydeveloper@gmail.com>
2022-05-28 20:28:53 -0700

    fixed delete button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
-  get 'posts/:id' => 'posts#show'
-  get 'posts/new' => 'posts#new'
-  post 'posts' => 'posts#create'
-  patch 'posts/:id' => 'posts#update'
-  delete 'posts/:id' => 'posts#destroy'
+  # get 'posts/:id' => 'posts#show'
+  # get 'posts/new' => 'posts#new'
+  # post 'posts' => 'posts#create'
+  # patch 'posts/:id' => 'posts#update'
+  # delete 'posts/:id' => 'posts#destroy'
   root 'posts#index'
 end
