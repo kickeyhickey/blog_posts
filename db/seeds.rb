57bd20b2400c144d4c39ceeb581ad4b5ccd7d56e
kickeyhickey <hickeydeveloper@gmail.com>
2022-05-29 00:40:04 -0700

    added full destroy func

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
+
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+blogs = [
+    {title: "yoyo", content:"hey hey"},
+    {title: "blueGreen", content:"colors"},
+    {title: "superman", content:"is better than Batman"}
+
+]
+
+blogs.each do |each_blog|
+    Post.create each_blog
+    puts "creating blog #{each_blog}"
+end
